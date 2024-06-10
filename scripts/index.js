7e7982d172a3aa3731e556f6b443c43917bba4ef
caloic <loic.cano@ynov.com>
2024-06-11 00:00:49 +0200

    Improve page styling, patch js country search error

--- scripts/index.js
+++ scripts/index.js
@@ -8,3 +8,21 @@ window.onscroll = function() {
     }
     prevScrollpos = currentScrollPos;
 }
+
+// Fonction pour afficher les boutons de pays
+function filterCountries() {
+    let input = document.getElementById('searchInput');
+    let filter = input.value.toUpperCase();
+
+    let form = document.getElementById('countriesForm');
+    let buttons = form.getElementsByClassName('country-button');
+
+    for (let i = 0; i < buttons.length; i++) {
+        let txtValue = buttons[i].textContent || buttons[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            buttons[i].style.display = "";
+        } else {
+            buttons[i].style.display = "none";
+        }
+    }
+}
